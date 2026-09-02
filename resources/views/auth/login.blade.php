47ba4ea57b2515d1d922d26024ea1565a968fdfc
DavinPramudia <davinpramudia30@gmail.com>
2026-09-02 14:56:27 +0700

    perbaikan lagi nambah script refresh dan penyesuaian

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -120,6 +120,12 @@
         </div>
 
     </div>
-
+    <script>
+        window.addEventListener('pageshow', function(event) {
+            if (event.persisted || (performance.navigation && performance.navigation.type === 2)) {
+                window.location.reload();
+            }
+        });
+    </script>
 </body>
 </html>
